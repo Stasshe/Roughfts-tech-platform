243edb7d4a2709d7564f5ec3b3b720e74c594660
Stasshe <133995608+Stasshe@users.noreply.github.com>
2025-02-13 14:07:20 +0900

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,9 +1,22 @@
+const isProd = process.env.NODE_ENV === 'production';
+
 module.exports = {
+  reactStrictMode: true,
+  trailingSlash: true,
+  assetPrefix: isProd ? 'https://roughfts-tech-platform.onrender.com/' : '',
+  images: {
+    unoptimized: true,
+  },
+  exportPathMap: async function () {
+    return {
+      '/': { page: '/' },
+    }
+  },
   compiler: {
-    styledComponents: true, // Styled Componentsのサポート
+    styledComponents: true,
   },
   webpack: (config) => {
-    config.resolve.extensions.push('.ts', '.tsx'); // ここを追加
+    config.resolve.extensions.push('.ts', '.tsx');
     config.module.rules.push({
       test: /\.(ts|tsx)$/,
       exclude: /node_modules/,
