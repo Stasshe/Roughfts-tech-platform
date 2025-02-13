bcfa1ba0d8fadc0f92fac79146473e4bd608c638
Stasshe <133995608+Stasshe@users.noreply.github.com>
2025-02-13 00:27:23 +0000

    update

--- next.config.js
+++ next.config.js
@@ -1,28 +1,27 @@
 module.exports = {
-    swcMinify: true,
-    compiler: {
-      styledComponents: true, // Styled Componentsのサポート
-    },
-    webpack: (config) => {
-      config.module.rules.push({
-        test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'swc-loader',
-          options: {
-            jsc: {
-              transform: {
-                react: {
-                  runtime: 'automatic',
-                  useBuiltIns: true,
-                },
+  swcMinify: true,
+  compiler: {
+    styledComponents: true, // Styled Componentsのサポート
+  },
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.(js|jsx)$/,
+      exclude: /node_modules/,
+      use: {
+        loader: 'swc-loader',
+        options: {
+          jsc: {
+            transform: {
+              react: {
+                runtime: 'automatic',
+                useBuiltIns: true,
               },
-              externalHelpers: true,
             },
+            externalHelpers: true,
           },
         },
-      });
-      return config;
-    },
-  };
-  
+      },
+    });
+    return config;
+  },
+};
