4eb2e994a2752654e2d43059bfd0fd243af8a75f
Stasshe <133995608+Stasshe@users.noreply.github.com>
2025-02-13 23:48:54 +0000

    favicon

---
+++ pages/_document.js
@@ -0,0 +1,21 @@
+// pages/_document.js
+import Document, { Html, Head, Main, NextScript } from 'next/document';
+
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html>
+        <Head>
+          <link rel="icon" href="../assets/ico.png" type="image/png" />
+          <link rel="apple-touch-icon" href="../assets/ico.png" />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
