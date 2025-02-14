f57f3f4ef87fcaa60b9bdef966099f3509f05fac
Stasshe <133995608+Stasshe@users.noreply.github.com>
2025-02-14 09:20:20 +0900

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -8,7 +8,8 @@ class MyDocument extends Document {
         <Head>
           <link rel="icon" href="/assets/ico.png" type="image/png" />
           <link rel="apple-touch-icon" href="/assets/ico.png" />
-          <link rel="shortcut icon" href="/assets/ico.png" type="image/png"/>
+          <link rel="shortcut icon" href="/assets/ico.png" type="image/png" />
+          <link rel="icon" href="/assets/ico_svg.svg" type="image/svg+xml" />
         </Head>
         <body>
           <Main />
