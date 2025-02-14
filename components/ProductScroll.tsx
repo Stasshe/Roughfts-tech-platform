5d0ee52bcc3453952d667c330c16986d43e9db56
Stasshe_roughfts <133995608+Stasshe@users.noreply.github.com>
2025-02-14 09:34:54 +0000

    0.4

--- components/ProductScroll.tsx
+++ components/ProductScroll.tsx
@@ -14,7 +14,7 @@ const ProductScroll = () => {
       <SectionTitle
         initial={{ opacity: 0, y: 20 }}
         animate={{ opacity: 1, y: 0 }}
-        transition={{ duration: 0.8 }}
+        transition={{ duration: 0.4 }}
       >
         {translations[language].sections.selectedWorks}
       </SectionTitle>
