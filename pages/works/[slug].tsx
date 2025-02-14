c122daf2d26ce90b652ffb061dc788a31f6ca6cb
Stasshe <133995608+Stasshe@users.noreply.github.com>
2025-02-14 09:42:43 +0900

    Update [slug].tsx

--- pages/works/[slug].tsx
+++ pages/works/[slug].tsx
@@ -339,8 +339,9 @@ const DemoSection = styled.div`
 
 const Lightbox = styled(motion.div)`
   position: fixed;
-  top: 0;
-  left: 0;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
   width: 100%;
   height: 100%;
   background: rgba(0, 0, 0, 0.9);
